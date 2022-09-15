a3fc39937c3e7f54b20632fc7092f643e65979fb
Ruan-Patrick <ruanp77@hotmail.com>
2022-09-15 11:32:45 -0300

    Initial commit

---
+++ repetições/ex013.js
@@ -0,0 +1,19 @@
+/*estrutura de repetição com teste logico no inicio*/
+/*var c = 1
+while (c<=500) {
+    console.log(`passo ${c}`)
+    c++ 
+}*/
+
+
+/*estrutura de repetição com teste logico no final*/
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++ 
+ } while (c<6)
+
+ /*var c=1
+for (var c=1;c<=10;c++) {
+    console.log(`passo ${c}`)
+} */
