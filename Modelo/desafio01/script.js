e64da4d975be956d212acb7fef963d5be7075781
Ruan-Patrick <ruanp77@hotmail.com>
2022-09-18 14:12:42 -0300

    segunda tentativa (não rodando)

--- Modelo/desafio01/script.js
+++ Modelo/desafio01/script.js
@@ -1,13 +1,14 @@
 function carregar() {
 var msg = window.document.getElementById('msg')
-var foto = window.document.getElementById('imagem')
+var img = window.document.getElementById ('imagem')
 var data = new date()
 var hora = data.getHours()
 msg.innerHTML = `agora são ${hora} horas.`}
-if (hora >= 0 && hora <12) {
-    Img.src = 'imagens/manhã pronta.png'
-} else if (hora >=12 && hora <18) {
+if (hora >= 0 && hora < 12) {
+    //Bom dia
 
+} else if (hora >=12 && hora <18) {
+//Boa tarde
 } else {
-    img.src = 'imagens/noite pronta.png'
+   //boa noite
 }
