2690072bd04ae6c7d9d91a2e2b9973e354aa1e62
Ruan-Patrick <ruanp77@hotmail.com>
2022-09-18 12:07:46 -0300

    criação de modelo pros desafios js (erro na var foto)

--- Modelo/desafio01/script.js
+++ Modelo/desafio01/script.js
@@ -1,1 +1,13 @@
-alert('Eae pega manga muleque doido,apara logo aquela ali pra mim')
+function carregar() {
+var msg = window.document.getElementById('msg')
+var foto = window.document.getElementById('imagem')
+var data = new date()
+var hora = data.getHours()
+msg.innerHTML = `agora são ${hora} horas.`}
+if (hora >= 0 && hora <12) {
+    Img.src = 'imagens/manhã pronta.png'
+} else if (hora >=12 && hora <18) {
+
+} else {
+    img.src = 'imagens/noite pronta.png'
+}
