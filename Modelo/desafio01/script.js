88d2c3977209878330174e6cb3d0d1edff825d72
Ruan-Patrick <ruanp77@hotmail.com>
2022-09-19 23:49:01 -0300

    resolução final

--- Modelo/desafio01/script.js
+++ Modelo/desafio01/script.js
@@ -1,14 +1,20 @@
 function carregar() {
-var msg = window.document.getElementById('msg')
-var img = window.document.getElementById ('imagem')
-var data = new date()
+var data = new Date()
 var hora = data.getHours()
-msg.innerHTML = `agora são ${hora} horas.`}
-if (hora >= 0 && hora < 12) {
-    //Bom dia
-
-} else if (hora >=12 && hora <18) {
-//Boa tarde
+var msg = document.getElementById('msg')
+var img = document.createElement('img')
+msg.innerHTML = `agora são ${hora} horas.`
+img.setAttribute('id', 'foto')
+if (hora>=0 && hora<12) {
+    img.setAttribute('src', 'manhãpronta.png')
+    document.body.style.background = '#d7e4f2'
+} else if (hora>=12 && hora<=18) {
+    img.setAttribute('src', 'tardepronta.png')
+    img.style.textAlign = 'center'
+    document.body.style.background = '#d48506'
 } else {
-   //boa noite
+    img.setAttribute('src', 'noitepronta.png')
+    document.body.style.background = '#021f55'
+}
+res.appendChild(img)
 }
