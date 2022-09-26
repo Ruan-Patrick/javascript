5efe917169fe4b06735240b613a8e2b3a5bdf904
Ruan-Patrick <ruanp77@hotmail.com>
2022-09-26 07:15:37 -0300

    calculadora

--- Modelo/desafio03/script.js
+++ Modelo/desafio03/script.js
@@ -8,13 +8,23 @@ function contar() {
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i =Number(ini.value)
         let f =Number(fim.value)
         let p =Number(passo.value)
-
-        for (let c = i; c <= f; c += p) {
-            res.innerHTML+= `${c} `
+        if (p <= 0) {
+            window.alert('passo inválido! considerando passo 1')
+            p = 1
         }
+        if (i < f) {
+            //contagem crescente
+            for (let c = i; c <= f; c += p)
+            res.innerHTML += ` ${c} \u{1F449}`
+        } else {
+            //contagem regressiva
+            for (let c = i; c >= f; c -= p)
+            res.innerHTML += ` ${c} \u{1F449}`
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
